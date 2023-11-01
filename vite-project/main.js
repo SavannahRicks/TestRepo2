f6e2a1b06de4fe6bd5ac16da837ff34bd5aca05d
Savannah Ricks <savannahricksofficial123@gmail.com>
2023-11-01 16:44:34 -0400

    pushing again to fix pages

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,7 +1,4 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.128.0/build/three.module.js';
-import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/GLTFLoader.js';
-
+import * as THREE from 'three';
 
 function main() {
     //scene set up
